4728835d61a815567d60fa26491fdefa93214a98
annaisabel93 <agalvao12@hotmail.com>
2016-03-28 18:32:01 +0100

    [UPDATE] Transporter-ws works; .wsdl added

--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
@@ -1,6 +1,53 @@
 package pt.upa.transporter.ws;
+import java.util.List;
 
-public class TransporterPort {
+import javax.jws.WebService;
+
+@WebService(
+    endpointInterface="pt.upa.transporter.ws.TransporterPortType",
+    wsdlLocation="transporter.1_0.wsdl",
+    name="TransporterWebService",
+    portName="TransporterPort",
+    targetNamespace="http://ws.transporter.upa.pt/",
+    serviceName="TransporterService"
+)
+public class TransporterPort implements TransporterPortType{
+
+	@Override
+	public String ping(String name) {
+		return "pong";
+	}
+
+	@Override
+	public JobView requestJob(String origin, String destination, int price)
+			throws BadLocationFault_Exception, BadPriceFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JobView decideJob(String id, boolean accept) throws BadJobFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JobView jobStatus(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<JobView> listJobs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearJobs() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	// TODO
 
