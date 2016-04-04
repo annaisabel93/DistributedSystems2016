42d6e0eba2167fc0b87eb6c9802daa29bbb29cf4
annaisabel93 <agalvao12@hotmail.com>
2016-04-04 14:36:23 +0100

    [ADD and UPDATE] added .gitignore and updated transporter client

--- transporter-ws-cli/src/main/java/pt/upa/transporter/TransporterClientApplication.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/TransporterClientApplication.java
@@ -1,9 +1,13 @@
 package pt.upa.transporter;
 
+import pt.upa.transporter.ws.cli.TransporterClient;
+
 public class TransporterClientApplication {
 
-	public static void main(String[] args) throws Exception {
-		System.out.println(TransporterClientApplication.class.getSimpleName() + " starting...");
-
-	}
+        public static void main(String[] args) throws Exception {
+                System.out.println(TransporterClientApplication.class.getSimpleName() + " starting...");
+                TransporterClient client = new TransporterClient("http://localhost:8081/transporter-ws/endpoint");
+                String result = client.ping("client");
+                System.out.println(result);
+        }
 }
--- transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
@@ -1,7 +1,71 @@
 package pt.upa.transporter.ws.cli;
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 
-public class TransporterClient {
+import java.util.List;
+import java.util.Map;
 
-	// TODO
+import javax.xml.ws.BindingProvider;
+
+import pt.upa.transporter.ws.BadJobFault_Exception;
+import pt.upa.transporter.ws.BadLocationFault_Exception;
+import pt.upa.transporter.ws.BadPriceFault_Exception;
+
+import pt.upa.transporter.ws.JobView;
+import pt.upa.transporter.ws.TransporterPortType;
+import pt.upa.transporter.ws.TransporterService;
+
+
+public class TransporterClient implements TransporterPortType{
+
+        TransporterService service;
+        TransporterPortType port;
+
+        public TransporterClient(String URL) {
+                service = new TransporterService();
+                port = service.getTransporterPort();
+
+                System.out.println("Setting endpoint address ...");
+                BindingProvider bindingProvider = (BindingProvider) port;
+                Map<String, Object> requestContext = bindingProvider.getRequestContext();
+                requestContext.put(ENDPOINT_ADDRESS_PROPERTY, URL);
+        }
+
+        @Override
+        public String ping(String name) {
+                return port.ping(name);
+        }
+        
+
+@Override
+        public JobView requestJob(String origin, String destination, int price)
+                        throws BadLocationFault_Exception, BadPriceFault_Exception {
+                // TODO Auto-generated method stub
+                return null;
+        }
+
+        @Override
+        public JobView decideJob(String id, boolean accept) throws BadJobFault_Exception {
+                // TODO Auto-generated method stub
+                return null;
+        }
+
+        @Override
+        public JobView jobStatus(String id) {
+                // TODO Auto-generated method stub
+                return null;
+        }
+
+        @Override
+        public List<JobView> listJobs() {
+                // TODO Auto-generated method stub
+                return null;
+        }
+
+        @Override
+        public void clearJobs() {
+                // TODO Auto-generated method stub
+
+        }
+        // TODO
 
 }
