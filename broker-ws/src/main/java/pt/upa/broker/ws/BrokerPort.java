05f350fa3b6cfb004cf46fa96ac9a0be7e13cc17
annaisabel93 <agalvao12@hotmail.com>
2016-04-11 14:27:31 +0100

    [UPDATE] Broker ping not working

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,11 +1,12 @@
 package pt.upa.broker.ws;
+import java.util.Collection;
 import java.util.List;
 
 import javax.jws.WebService;
-
-import org.apache.juddi.v3.client.transport.Transport;
+import javax.xml.registry.JAXRException;
 
 import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.upa.transporter.ws.cli.TransporterClient;
 
 @WebService(
     endpointInterface="pt.upa.broker.ws.BrokerPortType",
@@ -17,11 +18,32 @@ import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
 )
 
 public class BrokerPort implements BrokerPortType{
+	/** UDDI Naming instance for contacting UDDI server */
+	private UDDINaming uddiNaming = null;
 
+	/** Get UDDI Naming instance for contacting UDDI server */
+	UDDINaming getUddiNaming() {
+		return uddiNaming;
+	}
+	
 	@Override
 	public String ping(String name) {
-//		List<String> list = UDDINaming.list("UPATRANSPORT%");
-//		new Transport
+		String pings = "";
+		String ping = "";
+		Collection<String> urls;
+		try {
+			urls = uddiNaming.list("UPATRANSPORT%");
+			for(String url: urls){
+				TransporterClient client = new TransporterClient(url);
+				ping = client.ping(url);
+				pings = pings + ping;
+			}
+			
+		} catch (JAXRException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		// uddi.list("upatransp%") - usa-se o método list e vamos buscar tudo o que começa por upatransporter
 		//for (cada endereço recebido) 
 		//	new transporter client(URL)
