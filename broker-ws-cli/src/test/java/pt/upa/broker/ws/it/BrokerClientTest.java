cd25162edd9a6040e9b8bd0de85fe9812a1ece65
filiparsc <filipa.rscosta@gmail.com>
2016-04-15 23:08:07 +0100

    [NEW] BrokerClientTest empty

---
+++ broker-ws-cli/src/test/java/pt/upa/broker/ws/it/BrokerClientTest.java
@@ -0,0 +1,45 @@
+package pt.upa.broker.ws.it;
+
+import static org.junit.Assert.*;
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.Verifications;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BrokerClientTest {
+
+	//private static String wsURL = "http://host:port/endpoint";
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
+	//Map<String,Object> contextMap = null;
+
+	@Before
+	public void setUp() throws Exception {
+		//contextMap = new HashMap<String,Object>();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//contextMap = null;
+	}
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
