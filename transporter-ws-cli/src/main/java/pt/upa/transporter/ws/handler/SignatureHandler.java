55acce20f6bfa76edc56178e08acabafe5e27464
annaisabel93 <agalvao12@hotmail.com>
2016-05-10 16:33:36 +0100

    [UPDATE] added sign

--- transporter-ws-cli/src/main/java/pt/upa/transporter/ws/handler/SignatureHandler.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/ws/handler/SignatureHandler.java
@@ -20,32 +20,32 @@ import javax.xml.ws.handler.soap.SOAPMessageContext;
 import static javax.xml.bind.DatatypeConverter.printHexBinary;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Collection;
 
 //TODO falta fazer: quando é chamado ao enviar/receber/wtv -> se a mensagem for outbound vai ler, buscar bytes, resumir, e assinar - deve ter que ir buscar certificados!
-/**
-
+/*
  * #2 The client handler receives data from the client (via message context). #3
-
  * The client handler passes data to the server handler (via outbound SOAP
-
  * message header).
-
  *
-
  *
-
  * #10 The client handler receives data from the server handler (via inbound
-
  * SOAP message header). #11 The client handler passes data to the client (via
-
  * message context).
-
  *
-
  * *** GO BACK TO client to see what happens next! ***
-
  */
-
 
 public class SignatureHandler implements SOAPHandler<SOAPMessageContext>{
 
@@ -66,16 +66,12 @@ public class SignatureHandler implements SOAPHandler<SOAPMessageContext>{
 		if (outbound) {
 
 			// outbound message
-
 			// *** #2 ***
-
 			// get token from request context
-
 			String propertyValue = (String) smc.get(REQUEST_PROPERTY);
 			System.out.printf("%s received '%s'%n", CLASS_NAME, propertyValue);
-			
-			// put token in request SOAP header
 
+			// put token in request SOAP header
 			try {
 				// get SOAP envelope
 				SOAPMessage msg = smc.getMessage();
@@ -88,7 +84,6 @@ public class SignatureHandler implements SOAPHandler<SOAPMessageContext>{
 					sh = se.addHeader();
 
 				// add header element (name, namespace prefix, namespace)
-
 				Name name = se.createName(REQUEST_HEADER, "e", REQUEST_NS);
 				SOAPHeaderElement element = sh.addHeaderElement(name);
 
@@ -104,11 +99,11 @@ public class SignatureHandler implements SOAPHandler<SOAPMessageContext>{
 				System.out.printf("Failed to add SOAP header because of %s%n", e);
 
 			}
-			
+
 		} else {
 			// inbound message
 			// get token from response SOAP header
-			
+
 			try {
 
 				// get SOAP envelope header
@@ -156,7 +151,7 @@ public class SignatureHandler implements SOAPHandler<SOAPMessageContext>{
 		}
 		return true;
 	}
-	
+
 	public boolean handleFault(SOAPMessageContext smc) {
 		return true;
 	}
@@ -167,7 +162,7 @@ public class SignatureHandler implements SOAPHandler<SOAPMessageContext>{
 
 	public void close(MessageContext messageContext) {
 	}
-	
+
 	public void digest(String[] args) throws NoSuchAlgorithmException {
 		// check args and get plaintext
 
@@ -188,15 +183,227 @@ public class SignatureHandler implements SOAPHandler<SOAPMessageContext>{
 		MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
 		System.out.println(messageDigest.getProvider().getInfo());
 
-
-
 		System.out.println("Computing digest ...");
 		messageDigest.update(plainBytes);
 		byte[] digest = messageDigest.digest();
 
-
-
 		System.out.println("Digest:");
 		System.out.println(printHexBinary(digest));
+	}
+
+	final static String CERTIFICATE_FILE = "example.cer";
+	final static String KEYSTORE_FILE = "keystore.jks";
+	final static String KEYSTORE_PASSWORD = "1nsecure";
+	final static String KEY_ALIAS = "example";
+	final static String KEY_PASSWORD = "ins3cur3";
+
+	public static void sign(String[] args) throws Exception {
+		// check arguments and get plain-text
+
+		if (args.length != 1) {
+
+			System.err.println("args: (text)");
+			return;
+		}
+
+		final String plainText = args[0];
+		final byte[] plainBytes = plainText.getBytes();
+
+		System.out.println("Text:");
+		System.out.println(plainText);
+
+		System.out.println("Bytes:");
+		System.out.println(printHexBinary(plainBytes));
+
+		// make digital signature
+		System.out.println("Signing ...");
+
+		byte[] digitalSignature = makeDigitalSignature(plainBytes, getPrivateKeyFromKeystore(KEYSTORE_FILE,
+
+				KEYSTORE_PASSWORD.toCharArray(), KEY_ALIAS, KEY_PASSWORD.toCharArray()));
+
+		System.out.println("Signature Bytes:");
+		System.out.println(printHexBinary(digitalSignature));
+
+		Certificate certificate = readCertificateFile(CERTIFICATE_FILE);
+		PublicKey publicKey = certificate.getPublicKey();
+
+		// verify the signature
+		System.out.println("Verifying ...");
+		boolean isValid = verifyDigitalSignature(digitalSignature, plainBytes, publicKey);
+
+		if (isValid) {
+			System.out.println("The digital signature is valid");
+		} else {
+			System.out.println("The digital signature is NOT valid");
+		}
+
+		// data modification ...
+		plainBytes[3] = 12;
+		System.out.println("Tampered bytes: (look closely around the 7th hex character)");
+		System.out.println(printHexBinary(plainBytes));
+
+		// again verify the signature
+		System.out.println("Verifying again ...");
+		isValid = verifyDigitalSignature(digitalSignature, plainBytes, publicKey);
+
+		if (isValid) {
+			System.out.println("The digital signature is valid");
+
+		} else {
+			System.out.println("The digital signature is NOT valid");
+		}
+	}
+
+	/**
+	 * Returns the public key from a certificate
+	 * 
+	 * @param certificate
+	 * @return
+	 */
+
+	public static PublicKey getPublicKeyFromCertificate(Certificate certificate) {
+		return certificate.getPublicKey();
+	}
+
+	/**
+	 * Reads a certificate from a file
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+
+	public static Certificate readCertificateFile(String certificateFilePath) throws Exception {
+
+		FileInputStream fis;
+
+		try {
+			fis = new FileInputStream(certificateFilePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Certificate file <" + certificateFilePath + "> not fount.");
+			return null;
+		}
+
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		
+		if (bis.available() > 0) {
+			Certificate cert = cf.generateCertificate(bis);
+			return cert;
+			// It is possible to print the content of the certificate file:
+			// System.out.println(cert.toString());
+		}
+
+		bis.close();
+		fis.close();
+
+		return null;
+	}
+	
+	/**
+	 * Reads a collections of certificates from a file
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+
+	public static Collection<Certificate> readCertificateList(String certificateFilePath) throws Exception {
+		FileInputStream fis;
+
+		try {
+			fis = new FileInputStream(certificateFilePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Certificate file <" + certificateFilePath + "> not fount.");
+			return null;
+		}
+
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		@SuppressWarnings("unchecked")
+		Collection<Certificate> c = (Collection<Certificate>) cf.generateCertificates(fis);
+		fis.close();
+		return c;
+	}
+
+	/**
+	 * Reads a PrivateKey from a key-store
+	 * 
+	 * @return The PrivateKey
+	 * @throws Exception
+	 */
+
+	public static PrivateKey getPrivateKeyFromKeystore(String keyStoreFilePath, char[] keyStorePassword,
+
+			String keyAlias, char[] keyPassword) throws Exception {
+
+		KeyStore keystore = readKeystoreFile(keyStoreFilePath, keyStorePassword);
+		PrivateKey key = (PrivateKey) keystore.getKey(keyAlias, keyPassword);
+
+		return key;
+	}
+
+
+
+	/**
+	 * Reads a KeyStore from a file
+	 * 
+	 * @return The read KeyStore
+	 * @throws Exception
+	 */
+
+	public static KeyStore readKeystoreFile(String keyStoreFilePath, char[] keyStorePassword) throws Exception {
+
+		FileInputStream fis;
+
+		try {
+			fis = new FileInputStream(keyStoreFilePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Keystore file <" + keyStoreFilePath + "> not fount.");
+			return null;
+		}
+
+		KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+		keystore.load(fis, keyStorePassword);
+		return keystore;
+	}
+
+
+
+	/** auxiliary method to calculate digest from text and cipher it */
+
+	public static byte[] makeDigitalSignature(byte[] bytes, PrivateKey privateKey) throws Exception {
+		// get a signature object using the SHA-1 and RSA combo
+
+		// and sign the plain-text with the private key
+
+		Signature sig = Signature.getInstance("SHA1WithRSA");
+		sig.initSign(privateKey);
+		sig.update(bytes);
+		byte[] signature = sig.sign();
+
+		return signature;
+	}
+
+
+
+	/**
+	 * auxiliary method to calculate new digest from text and compare it to the
+	 * to deciphered digest
+	 */
+
+	public static boolean verifyDigitalSignature(byte[] cipherDigest, byte[] bytes, PublicKey publicKey)
+
+			throws Exception {
+		// verify the signature with the public key
+
+		Signature sig = Signature.getInstance("SHA1WithRSA");
+		sig.initVerify(publicKey);
+		sig.update(bytes);
+
+		try {
+			return sig.verify(cipherDigest);
+		} catch (SignatureException se) {
+			System.err.println("Caught exception while verifying signature " + se);
+			return false;
+		}
 	}
 }
