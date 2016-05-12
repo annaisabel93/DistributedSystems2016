d0317f230c4a87fea0ea8de57453e672ab698055
annaisabel93 <agalvao12@hotmail.com>
2016-05-12 18:37:28 +0100

    [ADD] exception

---
+++ ca-ws-cli/src/main/java/pt/upa/ca/ws/cli/CAClientException.java
@@ -0,0 +1,20 @@
+package pt.upa.ca.ws.cli;
+
+public class CAClientException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public CAClientException() {
+    }
+
+    public CAClientException(String message) {
+        super(message);
+    }
+
+    public CAClientException(Throwable cause) {
+        super(cause);
+    }
+
+    public CAClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
